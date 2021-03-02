7883096203943471d5ab13b8cec4d8e91f998672
Oggup <edifoo@gmail.com>
2021-03-02 01:51:48 -0800

    can not get posts to work but everything else seems to have been updated fine

--- db/seed.js
+++ db/seed.js
@@ -1,4 +1,14 @@
-const { client, getAllUsers, createUser } = require("./index");
+const {
+  client,
+  getAllUsers,
+  createUser,
+  updateUser,
+  getAllPosts,
+  createPost,
+  updatePost,
+  getPostsByUser,
+  getUserById,
+} = require("./index");
 
 async function createInitialUser() {
   try {
@@ -7,14 +17,20 @@ async function createInitialUser() {
     const albert = await createUser({
       username: "albert",
       password: "bertie99",
+      name: "Alberto",
+      location: "Moscow",
     });
     const sandra = await createUser({
       username: "sandra",
       password: "sandra123",
+      name: "Sandi",
+      location: "Bikini Bottom",
     });
     const glamgal = await createUser({
       username: "glamagal",
       password: "galmi123",
+      name: "Gertrude",
+      location: "Ciudad Juarez",
     });
     console.log(albert);
     console.log(sandra);
@@ -25,6 +41,20 @@ async function createInitialUser() {
     throw error;
   }
 }
+async function createInitialPosts() {
+  try {
+    const [albert, sandra, glamgal] = await getAllUsers();
+
+    await createPost({
+      authorId: albert.id,
+      title: "First Post",
+      content:
+        "This is my first post. I hope I love writing blogs as much as I love writing them.",
+    });
+  } catch (error) {
+    throw error;
+  }
+}
 
 async function dropTables() {
   try {
@@ -32,6 +62,7 @@ async function dropTables() {
 
     await client.query(`
       DROP TABLE IF EXISTS users;
+      DROP TABLE IF EXISTS posts;
     `);
 
     console.log("Finished dropping tables!");
@@ -49,13 +80,60 @@ async function createTables() {
       CREATE TABLE users (
         id SERIAL PRIMARY KEY,
         username varchar(255) UNIQUE NOT NULL,
-        password varchar(255) NOT NULL
+        password varchar(255) NOT NULL,
+        name VARCHAR(255) NOT NULL,
+        location VARCHAR(255) NOT NULL,
+        active BOOLEAN DEFAULT true
       );
+      CREATE TABLE posts (
+        id SERIAL PRIMARY KEY,
+        "authorId" INTEGER REFERENCES users(id),
+        title VARCHAR(255) NOT NULL,
+        content TEXT NOT NULL,
+        active BOOLEAN DEFAULT true
+     );
     `);
 
+    
     console.log("Finished building tables!");
   } catch (error) {
     console.error("Error building tables!");
+    throw error;
+  }
+}
+async function testDB() {
+  try {
+    console.log("Starting to test database...");
+
+    console.log("Calling getAllUsers");
+    const users = await getAllUsers();
+    console.log("Result:", users);
+
+    console.log("Calling updateUser on users[0]");
+    const updateUserResult = await updateUser(users[0].id, {
+      name: "Newname Sogood",
+      location: "Lesterville, KY",
+    });
+    console.log("Result:", updateUserResult);
+
+    console.log("Calling getAllPosts");
+    const posts = await getAllPosts();
+    console.log("Result:", posts);
+
+    console.log("Calling updatePost on posts[0]");
+    const updatePostResult = await updatePost(posts[0].id, {
+      title: "New Title",
+      content: "Updated Content",
+    });
+    console.log("Result:", updatePostResult);
+
+    console.log("Calling getUserById with 1");
+    const albert = await getUserById(1);
+    console.log("Result:", albert);
+
+    console.log("Finished database tests!");
+  } catch (error) {
+    console.log("Error during testDB");
     throw error;
   }
 }
@@ -67,21 +145,8 @@ async function rebuildDB() {
     await dropTables();
     await createTables();
     await createInitialUser();
+    await createInitialPosts();
   } catch (error) {
-    throw error;
-  }
-}
-
-async function testDB() {
-  try {
-    console.log("Starting to test database...");
-
-    const users = await getAllUsers();
-    console.log("getAllUsers:", users);
-
-    console.log("Finished database tests!");
-  } catch (error) {
-    console.error("Error testing database!");
     throw error;
   }
 }
@@ -90,3 +155,4 @@ rebuildDB()
   .then(testDB)
   .catch(console.error)
   .finally(() => client.end());
+
